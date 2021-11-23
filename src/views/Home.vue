5bf349e9962d477910eec7725e37e904fab75811
shiyuq <1543082564@qq.com>
2021-11-23 18:08:57 +0800

    🎨 gateway变化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@ export default {
   data() {
     return {
       userInfo: null,
-      gatewayUrl: "http://192.168.20.110:6001",
+      gatewayUrl: "http://142.192.173.195:6001",
     };
   },
   created() {
